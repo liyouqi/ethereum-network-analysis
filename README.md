ff7ab9cc73043330a8b64c10722d9b646343f049
dada <dada9081@gmail.com>
2025-11-15 18:49:23 +0100

    update(preprocess)

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 ## Setup Environment
 conda env create -f environment.yml
 conda activate ethereum-analysis
+
+conda env update -f environment.yml   //if you make changes to environment.yml
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,10 +1,29 @@
 import pandas as pd
-import os
+import os, sys
 
-PROCESSED_PATH = "data/processed/"
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, '..', '..'))
+
+PROCESSED_PATH = os.path.join(PROJECT_ROOT, 'data', 'processed')
+
+TOKEN_TX_STRING_COLS = [
+    "transaction_hash",
+    "from_address",
+    "to_address",
+    "block_number",
+    "token_address",
+    "raw_value",
+    "block_timestamp",
+    "user_value"
+]
 
 def load_clean_transactions():
-    return pd.read_csv(PROCESSED_PATH + "transactions_clean.csv")
+    return pd.read_csv(os.path.join(PROCESSED_PATH, "transactions_clean.csv"))
 
 def load_clean_token_transfers():
-    return pd.read_csv(PROCESSED_PATH + "token_transfers_clean.csv")
+    path = os.path.join(PROCESSED_PATH, "token_transfers_clean.csv")
+    return pd.read_csv(path, dtype={c: "string" for c in TOKEN_TX_STRING_COLS}, low_memory=False)
+
+
