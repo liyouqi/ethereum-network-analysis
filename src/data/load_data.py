7b20f16172776c20788e66f4c00c91fc319365c2
dada <dada9081@gmail.com>
2025-11-16 17:30:09 +0100

    update(ethereum analysis)

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -7,7 +7,9 @@ CURRENT_DIR = os.path.dirname(__file__)
 PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, '..', '..'))
 
 PROCESSED_PATH = os.path.join(PROJECT_ROOT, 'data', 'processed')
-
+RAW_PATH = os.path.join(PROJECT_ROOT, 'data', 'raw')
+# print("Processed data path:", PROCESSED_PATH)
+# print("Raw data path:", RAW_PATH)
 TOKEN_TX_STRING_COLS = [
     "transaction_hash",
     "from_address",
@@ -26,4 +28,5 @@ def load_clean_token_transfers():
     path = os.path.join(PROCESSED_PATH, "token_transfers_clean.csv")
     return pd.read_csv(path, dtype={c: "string" for c in TOKEN_TX_STRING_COLS}, low_memory=False)
 
-
+def load_raw_contracts():
+    return pd.read_csv(os.path.join(RAW_PATH, "contracts_filtered_export.csv"))
